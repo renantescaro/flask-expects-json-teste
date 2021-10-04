20696826abad60393d84d3d5af7dffd23dc1e430
renantescaro <renantescaro@yahoo.com.br>
2021-10-04 15:23:50 -0300

    adicionando validação de api e erro 400

--- flaskr/controllers/inicial_ctrl.py
+++ flaskr/controllers/inicial_ctrl.py
@@ -1,12 +1,15 @@
-from flask import render_template, jsonify, Blueprint
-from datetime import date, timedelta
+import json
+from flask import jsonify, render_template, Blueprint, make_response
+from flask_expects_json import expects_json
+from jsonschema import ValidationError
 
 bp = Blueprint(
     'inical',
     __name__,
     template_folder='templates' )
 
-class LogCtrl:
+
+class InicialCtrl:
     @bp.route('/', methods=['GET'])
     def inicial_template():
         return render_template(
@@ -14,6 +17,28 @@ class LogCtrl:
             titulo = 'inicial' )
 
 
-    @bp.route('/json', methods=['GET'])
-    def inicial_json():
-        return jsonify({'pagina':'inicial'})
+    schema = {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'email': {'type': 'string'},
+            'password': {'type': 'string'}
+        },
+        'required': ['email', 'password']
+    }
+    @bp.route('/requisicao', methods=['POST'])
+    @expects_json(schema)
+    def requisicao():
+        return json.dumps({'status':True})
+
+
+    @bp.errorhandler(400)
+    def erro(error):
+        if isinstance(error.description, ValidationError):
+            original_error = error.description
+            return make_response(
+                jsonify({
+                    'status'   : False,
+                    'mensagem' : original_error.message}
+                ), 400)
+        return error
