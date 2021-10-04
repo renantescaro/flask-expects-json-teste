38697db83876f4f1226629bc5d24d4c10ef3111b
renantescaro <renantescaro@yahoo.com.br>
2021-10-04 18:14:16 -0300

    alterando a porta

--- iniciar.bat
+++ iniciar.bat
@@ -1,7 +1,7 @@
-cd C:\Users\renan.tescaro\Desktop\flask\flask-wtf-teste
+cd C:\_python_projetos_\flask-expects-json-teste\
 
 set FLASK_APP=flaskr
 set FLASK_ENV=development
 set FLASK_RUN_HOST=0.0.0.0
-set FLASK_RUN_PORT=80
+set FLASK_RUN_PORT=5000
 flask run
